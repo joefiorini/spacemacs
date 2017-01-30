c7ec088f406e1d59c269dd9cee6432a9e1b0ff26
Jonathan del Strother <jdelStrother@gmail.com>
2017-01-30 13:17:40 +0000

    Colorize flow-eldoc via web-mode

--- layers/+lang/flow-type/funcs.el
+++ layers/+lang/flow-type/funcs.el
@@ -2,12 +2,35 @@
   (with-output-to-string
     (call-process-region (point-min) (point-max) shell-file-name nil standard-output nil shell-command-switch command)))
 
-(defun flow-type/type-description (info)
+(defun flow-type/get-info-from-def (info)
   (let ((type (alist-get 'type info)))
-    (if (string-equal type "(unknown)")
-        (let ((reasons (alist-get 'reasons info)))
-          (if (> (length reasons) 0) (alist-get 'desc (aref reasons 0))))
-      type)))
+    (if (not (string-equal type "(unknown)"))
+        type)))
+
+(defun flow-type/get-reasons-from-def (info)
+  (let ((reasons (alist-get 'reasons info)))
+    (if (> (length reasons) 0)
+        (alist-get 'desc (aref reasons 0)))))
+
+
+(defun flow-type/colorize-type (text)
+  ;; if web-mode is installed, use that to syntax highlight the type information
+  (if (configuration-layer/package-usedp 'web-mode)
+      (with-temp-buffer
+        (web-mode)
+        (insert text)
+        (web-mode-set-content-type "jsx")
+        (buffer-string))
+    ;; otherwise, just return the plain text
+    text))
+
+(defun flow-type/describe-info-object (obj)
+  (let ((info (flow-type/get-info-from-def obj)))
+    (if info
+        (flow-type/colorize-type info)
+      ;; even if the 'info' field is unknown, there's sometimes useful information in the 'reasons' structure.
+      (flow-type/get-reasons-from-def obj))))
+
 
 (defun flow-type/type-at-cursor ()
   (let ((output (flow-type/call-process-on-buffer-to-string
@@ -15,7 +38,7 @@
                          (executable-find "flow")
                          (line-number-at-pos) (+ (current-column) 1)))))
     (unless (string-match "\w*flow is still initializing" output)
-      (flow-type/type-description (json-read-from-string output)))))
+      (flow-type/describe-info-object (json-read-from-string output)))))
 
 (defun flow-type/enable-eldoc ()
   (if (and buffer-file-name (locate-dominating-file buffer-file-name ".flowconfig"))
